5e77793efa568ac9222da3e4f077964cb5631915
arathy456 <arathisurendranatkm@gmai..com>
2021-02-16 15:08:40 +0530

    changed

--- Hello.java
+++ Hello.java
@@ -3,5 +3,8 @@ public class Hello {
     public static void main(String[] args) {
         
 System.out.println("Arathy Surendran");
+System.out.println("Thonikadavil");
+System.out.println("Naranammoozhy");
+System.out.println("Athikayam");
     }
 }
